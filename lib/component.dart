e6e9b2aebdb2f056e2a2d460394c90fd026ae361
Roman Snitko <roman.snitko@gmail.com>
2016-04-11 19:36:25 +0300

    Get rid of unecessary code in Component, now provided by relevant packages

--- lib/component.dart
+++ lib/component.dart
@@ -1,27 +1,13 @@
 part of nest_ui;
 
-class Component {
-
-  Component parent;
+class Component extends Object with observable.Subscriber,
+                                    observable.Publisher,
+                                    HeritageTree,
+                                    Attributable
+{
 
   Map  behaviors = {};
-  Map  events    = {};
-  List roles     = [];
-  List <Component> children = [];
-
   Component() {
-  }
-
-  addChild(c) {
-    this.children.add(c);
-  }
-
-  removeChild(child_id, [Symbol id_type=#id]) {
-    if(id_type == #id) {
-      this.children.removeAt(child_id);
-    } else {
-      this.children.removeWhere((c) => c.roles.contains(child_id));
-    }
   }
 
 }
--- lib/nest_ui.dart
+++ lib/nest_ui.dart
@@ -1,3 +1,16 @@
 library very_ui;
 
+// vendor libs
+//import 'dart:html'   ;
+import 'dart:mirrors';
+import 'dart:math'   ;
+
+// local libs
+import 'package:observable_roles/observable_roles.dart' as observable;
+import 'package:heritage_tree/heritage_tree.dart';
+import 'package:attributable/attributable.dart';
+//import 'package:attributable/attributable.dart';
+//import 'package:validatable/validatable.dart';
+
+// parts of the current lib
 part 'component.dart';
