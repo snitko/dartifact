ea0c44fb13a89da3431d391ebf0c79de98ba356d
Roman Snitko <roman.snitko@gmail.com>
2016-08-01 21:46:10 +0700

    Extracting SelectComponent and SelectComponent behaviors from an actual nest_ui app and into the library

---
+++ lib/behaviors/select_component_behaviors.dart
@@ -0,0 +1,81 @@
+part of nest_ui;
+
+class SelectComponentBehaviors extends BaseComponentBehaviors {
+
+  Component   component;
+  HtmlElement options_container;
+  HtmlElement selectbox;
+  bool        options_container_hidden = true;
+  Map         scroll_pos_bottom = 0;
+
+  SelectComponentBehaviors(Component c) : super(c) {
+    this.component = c;
+    this.options_container = this.dom_element.querySelector('[data-component-part="options_container"');
+    this.selectbox         = this.dom_element.querySelector('[data-component-part="selectbox"');
+  }
+
+  toggle() {
+    if(options_container_hidden)
+      open();
+    else
+      close();
+  }
+
+  open() {
+    scroll_pos_bottom = component.lines_to_show-1;
+    component.opened = true;
+    selectbox.classes.add("open");
+    options_container.style.minWidth = "${pos.getDimensions(selectbox)['x'] - 2}px";
+    options_container.style.display = 'block';
+    options_container_hidden = false;
+    _applyLinesToShow();
+  }
+
+  close() {
+    component.opened = false;
+    selectbox.classes.remove("open");
+    options_container.style.display = 'none';
+    options_container_hidden = true;
+    _removeFocusFromOptions();
+  }
+
+  focusCurrentOption() {
+    _removeFocusFromOptions();
+    var current_option = this.options_container.querySelector(".option[data-option-value=\"${this.component.focused_option}\"");
+    current_option.classes.add("focused");
+    _scroll();
+  }
+
+  externalClickResponse() {
+    close();
+  }
+
+  _removeFocusFromOptions() {
+    this.options_container.querySelectorAll('.option').forEach((el) => el.classes.remove("focused"));
+  }
+
+  _applyLinesToShow() {
+    var opt_els = options_container.querySelectorAll('.option');
+    if(opt_els.isEmpty)
+      return;
+    var option_height = pos.getDimensions(opt_els[0])['y'];
+    if(component.lines_to_show > opt_els.length)
+      options_container.style.height = "${option_height*opt_els.length}px";
+    else
+      options_container.style.height = "${option_height*component.lines_to_show}px";
+  }
+  
+  _scroll() {
+    var option_height = pos.getDimensions(options_container.querySelector('.option'))['y'];
+    doScroll() => options_container.scrollTop = option_height*component.focused_option_id;
+    if(scroll_pos_bottom < component.focused_option_id) {
+      scroll_pos_bottom += 1;
+      doScroll();
+    } else if(scroll_pos_bottom-component.lines_to_show+1 > component.focused_option_id) {
+      scroll_pos_bottom -= 1;
+      doScroll();
+    }
+  }
+
+}
+
---
+++ lib/components/select_component.dart
@@ -0,0 +1,171 @@
+part of nest_ui;
+
+class SelectComponent extends Component {
+
+  List attribute_names = ["display_value", "input_value", "disabled"];
+  List native_events   = ["selectbox.click", "keypress", "keydown", "option.click"];
+  List behaviors       = [SelectComponentBehaviors];
+
+  SplayTreeMap options = new SplayTreeMap();
+  int lines_to_show = 10;
+
+  /** When user presses a key with selectbox focused,
+    * we put the character typed by this key into a stack which holds it there for some
+    * time - #keypress_stack_timeout - and inserts newly typed chars to the end of it.
+    * On each keypress, it finds the first match in the #options map and assigns it
+    * as the value. This basically mimicks the default browser behavior for the <select>
+    * element.
+    */
+  String keypress_stack = "";
+  int    keypress_stack_last_pdated = new DateTime.now().millisecondsSinceEpoch;
+  static const keypress_stack_timeout = 1;
+  String focused_option;
+  bool   opened = false;
+
+  Map attribute_callbacks = {
+    'default'  : (attr_name, self) => self.attribute_callbacks_collection['write_property_to_dom'](attr_name, self),
+    'disabled' : (attr_name, self) {
+      if(self.disabled == 'disabled') {
+        self.event_locks.add(#any); 
+        self.behave('disable');
+      } else {
+        self.behave('enable');
+        self.event_locks.remove(#any);
+      }
+    }
+  };
+
+  SelectComponent() {
+
+    // Workaround. Dart doesn't catch keydown events on divs, only on document -
+    // but, surprise, it corretly sets the target, so we can still get it!
+    var self = this;
+    document.onKeyDown.listen((e) {
+
+      if(self.event_locks.contains("keydown") || self.event_locks.contains(#any))
+        return;
+
+      if(e.target == self.dom_element && self.disabled != 'disabled' && [32,38,40,27,13].contains(e.keyCode)) {
+        switch(e.keyCode) {
+          case KeyCode.ESC:
+            self.behave('close');
+            break;
+          case KeyCode.UP:
+            self.opened ? focusPrevOption() : setPrevValue();
+            break;
+          case KeyCode.DOWN:
+            self.opened ? focusNextOption() : setNextValue();
+            break;
+          case KeyCode.ENTER:
+            self.setFocusedAndToggle();
+            break;
+          case KeyCode.SPACE:
+            self.setFocusedAndToggle();
+            break;
+        }
+        e.preventDefault();
+      }
+    });
+
+    event_handlers.add(event: 'click', role: 'self.selectbox', handler: (self,event) {
+      self.behave('toggle');
+    });
+    event_handlers.add(event: 'keypress', role: #self, handler: (self,event) {
+      var char = new String.fromCharCodes([event.charCode]);
+      updateKeypressStackWithChar(char);
+      setValueFromKeypressStack();
+    });
+    event_handlers.add(event: 'click', role: 'self.option', handler: (self,event) {
+      var t = event.target;
+      this.input_value   = t.getAttribute('data-option-value');
+      this.display_value = options[this.input_value];
+      self.behave('close');
+    });
+  }
+
+  after_initialize() {
+    super.after_initialize();
+    readOptionsFromDom();
+    updatePropertiesFromNodes(attrs: ["input_value", "disabled"], invoke_callbacks: true);
+    this.display_value = options[this.input_value];
+  }
+
+  readOptionsFromDom() {
+    var option_els = this.dom_element.querySelectorAll('[data-component-part="option"]');
+    for(var el in option_els)
+      options[el.getAttribute('data-option-value')] = el.text.trim();
+  }
+
+  getNextValue(String current) {
+    if(current == null)
+      return options.firstKey();
+    var key = options.firstKeyAfter(current);
+    if(key == null)
+      key = options.firstKey();
+    return key;
+  }
+  getPrevValue(String current) {
+    if(current == null)
+      return options.lastKey();
+    var key = options.lastKeyBefore(current);
+    if(key == null)
+      key = options.lastKey();
+    return key;
+  }
+  setNextValue() {
+    setValueByInputValue(getNextValue(this.input_value));
+  }
+  setPrevValue() {
+    setValueByInputValue(getPrevValue(this.input_value));
+  }
+  focusNextOption() {
+    this.focused_option = getNextValue(this.focused_option);
+    this.behave('focusCurrentOption');
+  }
+  focusPrevOption() {
+    this.focused_option = getPrevValue(this.focused_option);
+    this.behave('focusCurrentOption');
+  }
+  
+  setValueByInputValue(ip) {
+    this.input_value    = ip;
+    this.focused_option = ip;
+    this.display_value  = this.options[ip];
+  }
+
+  setValueFromKeypressStack() {
+    var found = false;
+    options.forEach((k,v) {
+      if(found)
+        return;
+      if(v.toLowerCase().startsWith(keypress_stack.toLowerCase())) {
+        this.setValueByInputValue(k);
+        found = true;
+      }
+    });
+  }
+
+  updateKeypressStackWithChar(String c) {
+    var time = new DateTime.now().millisecondsSinceEpoch;
+    if(keypress_stack_last_updated < time-(keypress_stack_timeout*1000)) {
+      this.keypress_stack_last_updated = time;
+      this.keypress_stack = c;
+    } else {
+      this.keypress_stack += c;
+    }
+  }
+
+  setFocusedAndToggle() {
+    if(this.opened) {
+      if(this.focused_option != null)
+        setValueByInputValue(this.focused_option);
+      this.focused_option = null;
+    }
+    this.behave('toggle');
+  }
+
+  get focused_option_id {
+    return options.keys.toList().indexOf(focused_option);
+  }
+
+}
