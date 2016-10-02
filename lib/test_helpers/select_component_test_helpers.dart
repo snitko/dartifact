dcc6c0703a4549160b92d60a36592f73e878a46e
Roman Snitko <roman.snitko@gmail.com>
2016-10-02 08:20:50 +0700

    Add createSelectComponent() and createEditableSelectComponent() helpers to select_component_test_helpers

--- lib/test_helpers/select_component_test_helpers.dart
+++ lib/test_helpers/select_component_test_helpers.dart
@@ -6,7 +6,7 @@ class TestSelectComponentBehaviors extends Mock {
 }
 
 class TestSelectComponent extends SelectComponent {
-  List behaviors = [TestEditableSelectComponentBehaviors];
+  List behaviors = [TestSelectComponentBehaviors];
   var http_request_completer;
   ajax_request(url) {
     http_request_completer = new Completer();
@@ -24,24 +24,54 @@ class TestEditableSelectComponent extends EditableSelectComponent {
   }
 }
 
-HtmlElement createEditableSelectElement({ roles: null, value: null, and: null }) {
+HtmlElement createEditableSelectElement({ roles: null, value: null, and: null, options: null }) {
+
+  if(options == null)
+    options = [];
+
   return createDomEl("TestEditableSelectComponent", roles: roles, and: (el) {
     return [
       createDomEl("", el: new InputElement(), part: "input", attr_properties: "input_value:value", attrs: { "value" : value }),
       createDomEl("", el: new InputElement(), part: "display_input", property: "display_input"),
       createDomEl("", part: "option_template"),
-      createDomEl("", part: "options_container")
+      createDomEl("", part: "options_container", and: (el) {
+        var opt_els = [];
+        options.forEach((o) {
+          opt_els.add(createDomEl("", part: "option", attrs: { "value": o }));
+        });
+        return opt_els;
+      })
     ];
   });
 }
 
-HtmlElement createSelectElement({ roles: null, value: null, and: null }) {
+HtmlElement createSelectElement({ roles: null, value: null, and: null, options: null }) {
+
+  if(options == null)
+    options = [];
+
   return createDomEl("TestSelectComponent", roles: roles, and: (el) {
     return [
       createDomEl("", el: new InputElement(), property: "input_value", attr_properties: "input_value:value", attrs: { "value" : value }),
       createDomEl("", el: new InputElement(), property: "display_value"),
       createDomEl("", part: "option_template"),
-      createDomEl("", part: "options_container")
+      createDomEl("", part: "options_container", and: (el) {
+        var opt_els = [];
+        options.forEach((o) {
+          opt_els.add(createDomEl("", part: "option", attrs: { "value": o }));
+        });
+        return opt_els;
+      })
     ];
   });
+}
+
+SelectComponent createSelectComponent({roles: null, options: null}) {
+  var el = createSelectElement(roles: roles, options: null);
+  return createComponent("TestSelectComponent", el: el, roles: roles);
+}
+
+SelectComponent createEditableSelectComponent({roles: null, options: null}) {
+  var el = createEditableSelectElement(roles: roles, options: null);
+  return createComponent("TestEditableSelectComponent", el: el, roles: roles);
 }
