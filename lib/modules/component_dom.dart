ae1492d82e5f14e85c2bab494d18033bfa0bee38
Roman Snitko <roman.snitko@gmail.com>
2016-10-03 11:29:23 +0700

    Refactoring: make Component#findPropertyElements() public, make it return a list of HtmlElement, not just the first one

--- lib/modules/component_dom.dart
+++ lib/modules/component_dom.dart
@@ -75,26 +75,53 @@ abstract class ComponentDom {
     }
   }
 
+  /** Finds property node in the DOM */
+  List<HtmlElement> findPropertyElements(String property_name) {
+    var property_els = allDomDescendantsAndSelfWithAttr(
+      this.dom_element,
+      attr_name: "data-component-property",
+      attr_value: property_name
+    );
+    if(property_els.isEmpty) {
+      property_els = allDomDescendantsAndSelfWithAttr(
+        this.dom_element,
+        attr_name: "data-component-attribute-properties",
+        // This one finds attribute properties of the format
+        // property_name:html_attribute_name_for_the_property
+        attr_value: new RegExp('(^|,| +)$property_name:')
+      );
+    }
+    return property_els;
+  }
+
+  HtmlElement findFirstPropertyElement(String property_name) {
+    var els = findPropertyElements(property_name);
+    if(!els.isEmpty)
+      return els.first;
+  }
+
   /** Updates dom element's #text or attribute so it refelects Component's current property value. */
   void prvt_writePropertyToNode(property_name) {
     if(this.dom_element == null)
       return;
-    var property_el = prvt_findPropertyEl(property_name);
-    if(property_el != null) {
-      var pa = property_el.attributes['data-component-attribute-properties'];
-      if(pa == null)
-        property_el.text = this.attributes[property_name].toString();
-      else {
-        var attr_property_name = prvt_getHtmlAttributeNameForProperty(pa, property_name);
-        if(this.attributes[property_name] == null) {
-          if(attr_property_name == "value")
-            property_el.value = null;
-          property_el.attributes.remove(attr_property_name);
-        }
+    var property_els = findPropertyElements(property_name);
+    for(var property_el in property_els) {
+      if(property_el != null) {
+        var pa = property_el.attributes['data-component-attribute-properties'];
+        if(pa == null)
+          property_el.text = this.attributes[property_name].toString();
         else {
-          property_el.setAttribute(attr_property_name, this.attributes[property_name].toString());
-          if(attr_property_name == "value")
-            property_el.value = this.attributes[property_name].toString();
+          var attr_property_name = prvt_getHtmlAttributeNameForProperty(pa, property_name);
+          if(this.attributes[property_name] == null) {
+            if(attr_property_name == "value")
+              property_el.value = null;
+            property_el.attributes.remove(attr_property_name);
+          }
+          else {
+            property_el.setAttribute(attr_property_name, this.attributes[property_name].toString());
+            if(attr_property_name == "value")
+              property_el.value = this.attributes[property_name].toString();
+          }
         }
       }
     }
@@ -102,7 +129,7 @@ abstract class ComponentDom {
 
   /** Reads property value from a DOM node, updates Component's object property with the value */
   void prvt_readPropertyFromNode(String property_name) {
-    var property_el = prvt_findPropertyEl(property_name);
+    var property_el = findFirstPropertyElement(property_name);
     if(property_el != null) {
       var pa = property_el.attributes['data-component-attribute-properties'];
       if(pa == null) {
@@ -127,25 +154,6 @@ abstract class ComponentDom {
       if(this.attributes[property_name] is String && this.attributes[property_name].isEmpty)
         this.attributes[property_name] = null;
     }
-  }
-
-  /** Finds property node in the DOM */
-  HtmlElement prvt_findPropertyEl(String property_name) {
-    var property_el = this.firstDomDescendantOrSelfWithAttr(
-      this.dom_element,
-      attr_name: "data-component-property",
-      attr_value: property_name
-    );
-    if(property_el == null) {
-      property_el = this.firstDomDescendantOrSelfWithAttr(
-        this.dom_element,
-        attr_name: "data-component-attribute-properties",
-        // This one finds attribute properties of the format
-        // property_name:html_attribute_name_for_the_property
-        attr_value: new RegExp('(^|,| +)$property_name:')
-      );
-    }
-    return property_el;
   }
 
   String prvt_getHtmlAttributeNameForProperty(String attr_list, String property_name) {
