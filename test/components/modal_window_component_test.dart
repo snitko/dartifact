f1c1dee624b3d0331763153dfd6b647531b7fc41
Roman Snitko <roman.snitko@gmail.com>
2016-11-03 22:57:32 +0700

    Correct paths for ModalWindowComponent files in the unit tests

--- test/components/modal_window_component_test.dart
+++ test/components/modal_window_component_test.dart
@@ -2,14 +2,13 @@ import "package:test/test.dart";
 import "dart:html";
 import "dart:async";
 import '../../lib/nest_ui.dart';
-import 'package:cookie/cookie.dart' as cookie;
 import 'package:mockito/mockito.dart';
 
-part '../../lib/components/hint_component.dart';
-part '../../lib/behaviors/hint_component_behaviors.dart';
+part '../../lib/components/modal_window_component.dart';
+part '../../lib/behaviors/modal_window_component_behaviors.dart';
 
 part '../../lib/test_helpers/component_test_helpers.dart';
-part '../../lib/test_helpers/hint_component_test_helpers.dart';
+part '../../lib/test_helpers/modal_window_component_test_helpers.dart';
 
 @TestOn("browser")
 
