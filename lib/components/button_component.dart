1a73e6a9e23ce474cffa62e611120490cbc5e86f
Roman Snitko <roman.snitko@gmail.com>
2016-11-10 03:56:13 +0700

    Do not lock Button if Button#lockable is false

--- lib/components/button_component.dart
+++ lib/components/button_component.dart
@@ -2,14 +2,23 @@ part of nest_ui;
 
 class ButtonComponent extends Component {
 
-  final List attribute_names = ["caption", "disabled"];
+  final List attribute_names = ["caption", "disabled", "lockable"];
         List native_events   = ["!click"];
         List event_lock_for  = ["click"];
+
+  Map default_attribute_values = { "lockable" : true };
 
   List behaviors = [ButtonComponentBehaviors];
 
   ButtonComponent() {
-    event_handlers.add(event: 'click', role: #self, handler: (self,event) => self.behave('lock'));
+    event_handlers.add(event: 'click', role: #self, handler: (self,event) {
+      if(self.lockable == true)
+        self.behave('lock');
+    });
+  }
+
+  afterInitialize() {
+    updatePropertiesFromNodes(attrs: ["lockable"], invoke_callbacks: true);
   }
 
 }
