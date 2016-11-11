8de648897e1e3915f9a927729920c204128f20d1
Roman Snitko <roman.snitko@gmail.com>
2016-11-12 02:30:09 +0700

    Fix: clone ajax_indicator for the button, put it next to the button in DOM

--- lib/behaviors/button_component_behaviors.dart
+++ lib/behaviors/button_component_behaviors.dart
@@ -6,7 +6,7 @@ class ButtonComponentBehaviors extends BaseComponentBehaviors {
   Element   ajax_indicator;
 
   ButtonComponentBehaviors(Component c) : super(c) {
-    this.ajax_indicator = querySelector(".ajaxIndicator");
+    this.ajax_indicator = querySelector(".ajaxIndicator").clone(true);
     this.component = c;
   }
 
@@ -19,8 +19,10 @@ class ButtonComponentBehaviors extends BaseComponentBehaviors {
   }
 
   lock() {
+    this.dom_element.parent.insertBefore(this.ajax_indicator, this.dom_element);
     if(this.ajax_indicator != null) {
       this.ajax_indicator.style.display = 'block';
+      this.ajax_indicator.style.position = 'absolute';
       pos.placeBy(this.ajax_indicator, this.dom_element, top: 0.5, left: 0, gravity_top: 0.5, gravity_left: 0.5 );
     }
     super.lock();
