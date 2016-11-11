38a41fdec869a9e262bdd63763651847f7a9adc7
Roman Snitko <roman.snitko@gmail.com>
2016-11-12 05:57:25 +0700

    Fix: ButtonComponent ajax_indicator handling

--- lib/behaviors/button_component_behaviors.dart
+++ lib/behaviors/button_component_behaviors.dart
@@ -6,7 +6,9 @@ class ButtonComponentBehaviors extends BaseComponentBehaviors {
   Element   ajax_indicator;
 
   ButtonComponentBehaviors(Component c) : super(c) {
-    this.ajax_indicator = querySelector(".ajaxIndicator").clone(true);
+    var ai = querySelector(".ajaxIndicator");
+    if(ai != null)
+      this.ajax_indicator = ai.clone(true);
     this.component = c;
   }
 
@@ -19,8 +21,8 @@ class ButtonComponentBehaviors extends BaseComponentBehaviors {
   }
 
   lock() {
-    this.dom_element.parent.insertBefore(this.ajax_indicator, this.dom_element);
     if(this.ajax_indicator != null) {
+      this.dom_element.parent.insertBefore(this.ajax_indicator, this.dom_element);
       this.ajax_indicator.style.display = 'block';
       this.ajax_indicator.style.position = 'absolute';
       pos.placeBy(this.ajax_indicator, this.dom_element, top: 0.5, left: 0, gravity_top: 0.5, gravity_left: 0.5 );
