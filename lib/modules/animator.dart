9f76a66f62d6558149aac2ccd759b609096176ba
Roman Snitko <roman.snitko@gmail.com>
2017-02-04 08:52:03 +0700

    Enable Animator to accept Lists of HtmlElements

--- lib/modules/animator.dart
+++ lib/modules/animator.dart
@@ -2,35 +2,60 @@ part of nest_ui;
 
 class Animator {
 
-  Future show(el, ms, { display_value: "block" }) {
-    if(el.offsetHeight > 0 && el.offsetParent == null)
-      return;
-
-    el.style.opacity = "0";
-    el.style.display = display_value;
-    var f = animate(el, properties: { "opacity": 1 }, duration: ms).onComplete.first;
-    return f;
+  Future show(elements, ms, { display_value: "block" }) {
+    return _applyToCollection(elements, (el) {
+      if(el.offsetHeight > 0 && el.offsetParent == null)
+        return;
+      el.style.opacity = "0";
+      el.style.display = display_value;
+      return animate(el, properties: { "opacity": 1 }, duration: ms).onComplete.first;
+    });
   }
 
-  Future hide(el, ms) {
-    var f = animate(el, properties: { "opacity": 0 }, duration: ms).onComplete.first;
-    f.then((i) => el.style.display = "none");
-    return f;
+  Future hide(elements, ms) {
+    return _applyToCollection(elements, (el) {
+      var animation = animate(el, properties: { "opacity": 0 }, duration: ms).onComplete.first;
+      animation.then((i) => el.style.display = "none");
+      return animation;
+    });
   }
 
-  Future scrollDown(el, ms, { display_value: "block" }) {
-    el.style.opacity = "0";
-    el.style.display = display_value;
-    var original_height = el.getBoundingClientRect().height;
-    el.style.height = "0px";
-    el.style.opacity = "1";
-    var f = animate(el, properties: { "height": "${original_height}px" }, duration: ms).onComplete.first;
-    return f;
+  Future scrollDown(elements, ms, { display_value: "block" }) {
+    return _applyToCollection(elements, (el) {
+      if(el.offsetHeight > 0 && el.offsetParent == null)
+        return;
+      el.style.opacity = "0";
+      el.style.display = display_value;
+      var original_height = el.getBoundingClientRect().height;
+      el.style.height = "0px";
+      el.style.opacity = "1";
+      return animate(el, properties: { "height": "${original_height}px" }, duration: ms).onComplete.first;
+    });
   }
 
-  Future scrollUp(el, ms) {
-    var f = animate(el, properties: { "height": "0px" }, duration: ms).onComplete.first;
-    f.then((i) => el.style.display = "none");
+  Future scrollUp(elements, ms) {
+    return _applyToCollection(elements, (el) {
+      var animation = animate(el, properties: { "height": "0px" }, duration: ms).onComplete.first;
+      animation.then((i) => el.style.display = "none");
+      return animation;
+    });
+  }
+
+  _applyToCollection(elements, func) {
+    if(!(elements is List))
+      elements = [elements];
+
+    elements = elements.map((el) {
+      if(el is Component)
+        return el.dom_element;
+      else
+        return el;
+    });
+
+    var f;
+    elements.forEach((el) {
+      f = func(el);
+    });
     return f;
   }
 
