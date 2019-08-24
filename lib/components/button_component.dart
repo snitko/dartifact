9dc31da8f51428b2aa02de22fb5d6920c536a03f
Roman Snitko <rs@hodlhodl.com>
2019-08-24 23:24:27 +0000

    Fix: ButtonComponent issue when touch ends on another element

--- lib/components/button_component.dart
+++ lib/components/button_component.dart
@@ -3,7 +3,7 @@ part of dartifact;
 class ButtonComponent extends Component {
 
   final List attribute_names = ["caption", "disabled", "lockable"];
-        List native_events   = ["!${Component.click_event}"];
+        List native_events   = ["!${Component.click_event}", "touchstart"];
         List event_lock_for  = [Component.click_event];
 
   Map default_attribute_values = { "lockable" : true, "disabled" : false };
@@ -11,9 +11,20 @@ class ButtonComponent extends Component {
   List behaviors = [ButtonComponentBehaviors];
 
   ButtonComponent() {
-    event_handlers.add(event: Component.click_event, role: #self, handler: (self,event) {
-      if(self.lockable == true)
-        self.behave('lock');
+    event_handlers.add(event: "click", role: #self, handler: (self,event) {
+      if(self.lockable == true) { self.behave('lock'); }
+    });
+
+    event_handlers.add(event: "touchend", role: #self, handler: (self,event) {
+
+      var pos = new PositionManager();
+      var el_start = pos.getPosition(event.target);
+      var el_end   = pos.getDimensions(event.target);
+      var point    = event.changedTouches[0].page;
+
+      if(point.x >= el_start["x"] && point.x <= el_end["x"] &&
+         point.y >= el_start["y"] && point.y <= el_end["y"] &&
+         self.lockable == true) { self.behave('lock'); }
     });
 
     this.attribute_callbacks["disabled"] = (attr_name,self) {
